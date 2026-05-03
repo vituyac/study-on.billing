1d9221a17fc9cf3c3f755a234fe008ab95f78204
vituyac <skisterev78@mail.ru>
2026-05-03 19:32:48 +0300

    feat: payment service

---
+++ src/Service/PaymentService.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Course;
+use App\Entity\Transaction;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+class PaymentService
+{
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+    ) {
+    }
+
+    public function deposit(User $user, int $amount): Transaction
+    {
+        if ($amount <= 0) {
+            throw new \InvalidArgumentException();
+        }
+
+        return $this->em->wrapInTransaction(function () use ($user, $amount): Transaction {
+            $transaction = new Transaction();
+            $transaction->setCustomer($user);
+            $transaction->setType(Transaction::TYPES['DEPOSIT']);
+            $transaction->setAmount($amount);
+
+            $user->setBalance(($user->getBalance() ?? 0) + $amount);
+
+            $this->em->persist($transaction);
+
+            return $transaction;
+        });
+    }
+
+    public function pay(User $user, Course $course): Transaction
+    {
+        if (($user->getBalance() ?? 0) < $course->getPrice()) {
+            throw new \DomainException();
+        }
+
+        return $this->em->wrapInTransaction(function () use ($user, $course): Transaction {
+            $transaction = new Transaction();
+            $transaction->setCustomer($user);
+            $transaction->setCourse($course);
+            $transaction->setType(Transaction::TYPES['PAYMENT']);
+            $transaction->setAmount($course->getPrice());
+
+            if ($course->getType() === 'RENT') {
+                $transaction->setExpiresAt((new \DateTimeImmutable())->modify('+7 days'));
+            }
+
+            $user->setBalance($user->getBalance() - $course->getPrice());
+
+            $this->em->persist($transaction);
+
+            return $transaction;
+        });
+    }
+}
